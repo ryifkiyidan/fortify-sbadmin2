ae9a9f9d71204eec2650e2ca59e41dc1b5d07210
ryifkiyidan <62694926+ryifkiyidan@users.noreply.github.com>
2021-03-28 16:33:09 +0700

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/blank', function () {
+    return view('blank');
+})->name('blank');
